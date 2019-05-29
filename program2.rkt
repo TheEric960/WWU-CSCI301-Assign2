06691a8ca1f9b72947033be12b6bcddbc4804a7c
Jack Wilson <wilso313@wwu.edu>
2019-05-28 22:05:05 -0700

    parse program input method completed

--- program2.rkt
+++ program2.rkt
@@ -1,12 +1,26 @@
 
 (define read-input-file
   (lambda (name)
-         (let ((p (open-input-file name)))
-           (define read-file
-             (lambda (x)
-               (if (eof-object? x)
-                   (begin
-                     (close-input-port p)
-                     '())
-                   (cons x (read-file (read p))))))
-           (read-file (read p)))))
+    (let ((p (open-input-file name)))
+      (define read-file
+        (lambda (x)
+          (if (eof-object? x)
+              (begin
+                (close-input-port p)
+                '())
+              (cons x (read-file (read-char p))))))
+      (read-file (read-char p)))))
+
+(define basic-parse
+  (lambda (name)
+    (define read-next
+      (lambda (word word-ls str-ls)
+        (cond ((null? str-ls) word-ls)
+              ((or (char=? #\return (car (string->list (car str-ls))))
+                   (char=? #\newline (car (string->list (car str-ls)))))
+               (read-next "" word-ls (cdr str-ls)))
+              ((char-whitespace? (car (string->list (car str-ls))))
+               (read-next "" (append word-ls (list word)) (cdr str-ls)))
+              (else (read-next (string-append word (car str-ls)) word-ls (cdr str-ls))))))
+    (read-next "" '() (map string (read-input-file "code")))))
+
